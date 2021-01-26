c68006ba7a04bde3c31830174ee7e4ae800feefe
israeljoab <israeljoab@gmail.com>
2021-01-26 06:28:34 -0800

    Pasta renomeada

--- aula3/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Israel 'Joab'");
-console.log('Israel "Joab"');
-console.log(`'Israel' "Joab"`);
-
-console.log(12345);
---
+++ sintaxe-basica/index.js
@@ -0,0 +1,7 @@
+console.log("Israel 'Joab'");
+console.log('Israel "Joab"');
+console.log(`'Israel' "Joab"`);
+
+console.log(12345);
+
+console.log('Meu nome é "Israel Joab". Estou aprendendo JavaScript às 05 horas da manhã.');
