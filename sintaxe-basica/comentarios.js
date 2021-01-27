abc750004f26536c0f90d473af61c12b8755f34e
israeljoab <israeljoab@gmail.com>
2021-01-27 05:58:13 -0300

    Atualizações

---
+++ sintaxe-basica/comentarios.js
@@ -0,0 +1,13 @@
+// Comentário
+console.log('Olá mundo!'); // Outro Comentário
+console.log('Linha 1');
+//console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// vai ser ignorado
+
+
+/* 
+Um Blodo de código
+Linha de comentário.
+*/
