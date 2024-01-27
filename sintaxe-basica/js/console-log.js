9af4843d608ca4583727c167e253c4a008ce1b88
Israel Joab <israeljoab@gmail.com>
2024-01-27 15:29:39 -0300

    Delete sintaxe-basica directory

--- sintaxe-basica/js/console-log.js
+++
@@ -1,7 +0,0 @@
-console.log("Israel 'Joab'");
-console.log('Israel "Joab"');
-console.log(`'Israel' "Joab"`);
-
-console.log(12345);
-
-console.log('Meu nome é "Israel Joab". Estou aprendendo JavaScript às 05 horas da manhã.');
